a894d01a8075eb56b2333a709ef28e3473f08db3
iyolaoyabiyi <iyolaoyabiyi@gmail.com>
2024-11-18 03:18:38 +0100

    feat: add functionality to save quote history

--- script.js
+++ script.js
@@ -2,25 +2,36 @@ $(document).ready(async () => {
   // get quotes
   const data = await fetch("./quotes.json");
   const quotes = await data.json();
+  const quotesLen = quotes.length;
+  const currentHistory = [];
+  // create an array for full history
   const unseenQuotes = JSON.parse(JSON.stringify(quotes));
-  // Function to get a random quote
-  const getRandomQuote = (quotes, index) => {
-    return quotes[index];
+  // Function to get a random number
+  const getRandomNum = (history, length) => {
+    let num = Math.floor(Math.random() * length);
+    while (history.includes(num)) {
+      num = Math.floor(Math.random() * length);
+    }
+    return num;
   }
+  // TODO:
+  // Show full history of generated quotes
+
   // show random quote on button click
   $('#new-quote').click(() => {
-    const quotesLen = unseenQuotes.length;
-    if (quotesLen < 1) {
-      unseenQuotes.push(...JSON.parse(JSON.stringify(quotes)))
+    const historyLen = currentHistory.length;
+    if (historyLen === quotesLen) {
+      currentHistory.splice(0, historyLen);
     }
     
-    const randomNum = Math.floor(Math.random() * quotesLen);
-    const quote = getRandomQuote(unseenQuotes, randomNum);
-
-    unseenQuotes.splice(randomNum, 1);
+    const randomNum = getRandomNum(currentHistory, quotesLen);
+    const quote = quotes[randomNum];
+    
+    currentHistory.push(randomNum);
+    // update full history
     
     $('#text').text(quote.quote);
-    $('#author').text(quote.author);
+    $('#author').text(`- ${quote.author}`);
     $('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${$('#text').text()}`);
   })
 })
